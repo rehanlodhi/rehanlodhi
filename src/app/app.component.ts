b1567e7dd362442667c6656bde16eddd4bffaa1c
Rehan Lodhi <rehanlodhi@live.com>
2024-02-07 12:21:10 +0500

    Hero setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavComponent} from "./nav/nav.component";
+import {HeroComponent} from "./hero/hero.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent, HeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
